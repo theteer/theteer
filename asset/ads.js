60be86ebfb59b6f7cf66f8a0a8fce60efc0f4a5f
theteer <57169215+theteer@users.noreply.github.com>
2020-08-30 22:59:45 +0530

    up

--- asset/ads.js
+++
@@ -1,8 +0,0 @@
-window.googletag = window.googletag || {cmd: []};
-googletag.cmd.push(function() {
-	googletag.defineSlot('3572644856',[728, 90],'ads')
-	.setTargeting('test', 'lazyload')
-	.addService(googletag.pubads());
-	googletag.pubads().enableLazyLoad();
-	googletag.enableServices();
-});
