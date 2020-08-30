786c3d142194410ae669303d75c6a071f653619c
theteer <57169215+theteer@users.noreply.github.com>
2020-08-30 22:32:46 +0530

    Update ads.js

--- asset/ads.js
+++ asset/ads.js
@@ -1,6 +1,6 @@
 window.googletag = window.googletag || {cmd: []};
 googletag.cmd.push(function() {
-	googletag.defineSlot('3572644856','ads')
+	googletag.defineSlot('3572644856',[728, 90],'ads')
 	.setTargeting('test', 'lazyload')
 	.addService(googletag.pubads());
 	googletag.pubads().enableLazyLoad();
